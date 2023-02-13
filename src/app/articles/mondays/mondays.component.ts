82d42aaad7afed7e0c202e3f7b4a28ee98849db7
Jesse <jesse@jessefitz.me>
2023-02-13 06:05:23 -0500

    mondays

---
+++ src/app/articles/mondays/mondays.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mondays',
+  template: `<markdown src="assets/article-content/mondays.md"></markdown>`,
+  styles: [
+  ]
+})
+export class MondaysComponent {
+
+}

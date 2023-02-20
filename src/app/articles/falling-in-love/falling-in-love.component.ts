f98980444c3183b05f493dcc274f19209ac0d3bf
Jesse <jesse@jessefitz.me>
2023-02-20 07:18:47 -0500

    falling in love

---
+++ src/app/articles/falling-in-love/falling-in-love.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-falling-in-love',
+  template: `<markdown src="assets/article-content/her.md"></markdown>`,
+  styles: [
+  ]
+})
+export class FallingInLoveComponent {
+
+}

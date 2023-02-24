eb1d05b79391d8788b61cb14892d829156d1b339
Jesse <jesse@jessefitz.me>
2023-02-24 05:55:40 -0500

    vanity

---
+++ src/app/articles/vanity/vanity.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vanity',
+  template: `<markdown src="assets/article-content/vanity.md"></markdown>`,
+  styles: [
+  ]
+})
+export class VanityComponent {
+
+}

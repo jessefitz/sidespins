498edf03e32feac5f179ceabc33e68cbb1a2b455
Jesse <jesse@jessefitz.me>
2023-03-11 09:06:03 -0500

    no shame

---
+++ src/app/articles/no-shame/no-shame.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-no-shame',
+  template: `<markdown src="assets/article-content/no-shame.md"></markdown>`,
+  styles: [
+  ]
+})
+export class NoShameComponent {
+
+}
